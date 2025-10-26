a20801f7dbba5b4ea51151e5087645b080a486c4
kuze15 <gonzalesvimbo61@gmail.com>
2025-10-26 19:22:14 +0800

    vimbo
    vimbo123

---
+++ Pages/vimbi1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Apartment.Pages
+{
+    public class vimbi1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
