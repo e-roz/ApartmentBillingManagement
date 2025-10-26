e7f13b8ecc2998e09776dc58afd3b02c5d5d1046
e-roz <137011618+e-roz@users.noreply.github.com>
2025-10-26 19:21:02 +0800

    Resolve merge conflicts in ApartmentModel.cs, AdminDashBoard.cshtml, and _Layout.cshtml

--- Migrations/20251026080726_AddApartmentEntity.Designer.cs
+++ Migrations/20251026080726_AddApartmentEntity.Designer.cs
@@ -4,6 +4,7 @@ using Apartment.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Apartment.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251026080726_AddApartmentEntity")]
+    partial class AddApartmentEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
